73d926f4533d768adf96ff4a0410549eabe6172c
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-05-23 14:56:16 +0300

    add pic

--- lib/main.dart
+++ lib/main.dart
@@ -34,53 +34,66 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start, // Align to the top
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
-          Container(
-            alignment: Alignment.topCenter,
-            margin: EdgeInsets.only(top: 40), // Increased top margin
-            child: Image.asset('images/FinalLogo.png',
-                width: 500, height: 300), // Adjusted size
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start, // Align to the top
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.only(top: 40), // Increased top margin
+                child: Image.asset('images/FinalLogo.png',
+                    width: 500, height: 300), // Adjusted size
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to PillPoppin!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24, // Adjust the font size as needed
+                  fontWeight: FontWeight
+                      .bold, // You can adjust the font weight too if needed
+                ),
+              ),
+              Text(
+                'keep your health in check,\n one pill at a time!',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 86, 87, 88),
+                  fontFamily: 'Georgia', // Use Georgia font
+                  fontSize: 20, // Adjust the font size as needed
+                  fontStyle: FontStyle.italic, // Apply italic style
+                ),
+                textAlign: TextAlign.center, // Center align the text
+              ),
+              SizedBox(height: 20), // Adjust the space between text and button
+              ElevatedButton(
+                onPressed: () {
+                  // Add navigation to next screen or functionality here
+                },
+                style: ElevatedButton.styleFrom(
+                  alignment: Alignment.bottomCenter,
+                  backgroundColor:
+                      Theme.of(context).colorScheme.secondary, // Pastel Pink
+                  foregroundColor:
+                      Theme.of(context).colorScheme.onSecondary, // White
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  textStyle: TextStyle(fontSize: 16),
+                ),
+                child: Text('Let\'s get started!'),
+              ),
+              SizedBox(height: 50), // Add some bottom margin if needed
+            ],
           ),
-          SizedBox(height: 20),
-          Text(
-            'Welcome to PillPoppin!',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 24, // Adjust the font size as needed
-              fontWeight: FontWeight
-                  .bold, // You can adjust the font weight too if needed
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              'images/Doctor1.png', // Replace 'your_photo.png' with your actual photo path
+              width: 250, // Adjust the width as needed
+              height: 250, // Adjust the height as needed
             ),
           ),
-          Text(
-            'keep your health in check,\n one pill at a time!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 86, 87, 88),
-              fontFamily: 'Georgia', // Use Georgia font
-              fontSize: 20, // Adjust the font size as needed
-              fontStyle: FontStyle.italic, // Apply italic style
-            ),
-            textAlign: TextAlign.center, // Center align the text
-          ),
-          SizedBox(height: 20), // Adjust the space between text and button
-          ElevatedButton(
-            onPressed: () {
-              // Add navigation to next screen or functionality here
-            },
-            style: ElevatedButton.styleFrom(
-              alignment: Alignment.bottomCenter,
-              backgroundColor:
-                  Theme.of(context).colorScheme.secondary, // Pastel Pink
-              foregroundColor:
-                  Theme.of(context).colorScheme.onSecondary, // White
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              textStyle: TextStyle(fontSize: 16),
-            ),
-            child: Text('Let\'s get started!'),
-          ),
-          SizedBox(height: 50), // Add some bottom margin if needed
         ],
       ),
     );
