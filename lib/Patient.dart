3547c8618888ad4bbc60039011d16ab5f60af8c2
Ghaidaaazzam <gidaa.azzam@gmail.com>
2024-05-24 10:55:26 +0300

    update patient details

--- lib/Patient.dart
+++ lib/Patient.dart
@@ -2,13 +2,14 @@ class Patient {
   String id;
   String fname;
   String lname;
+  String password;
   int age;
 
   // Constructor
-  Patient({
-    required this.id,
-    required this.fname,
-    required this.lname,
-    required this.age,
-  });
+  Patient(
+      {required this.id,
+      required this.fname,
+      required this.lname,
+      required this.age,
+      required this.password});
 }
