e507cc62874d09a5876b16817f61dd34213f6401
Ghaidaaazzam <gidaa.azzam@gmail.com>
2024-05-30 16:30:25 +0300

    user survey design

--- lib/survey.dart
+++ lib/survey.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'LogInPage.dart'; // Import the LoginPage
+import 'package:flutter_localizations/flutter_localizations.dart'; // Import the localization packages
 
 void main() {
-  runApp(MyApp());
+  runApp(PillPoppinApp());
 }
 
-class MyApp extends StatelessWidget {
+class PillPoppinApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,6 +21,20 @@ class MyApp extends StatelessWidget {
       locale: const Locale(
           'en', 'GB'), // Set the locale to UK English for 24-hour format
       theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFFA8DADC), // Pastel Blue
+          secondary: Color(0xFFFF9DC6), // Pastel Pink
+          background: Color.fromARGB(255, 217, 242, 255), // White
+          surface: Color.fromARGB(255, 175, 227, 252), // Pastel Bright Blue
+          onPrimary: Color(0xFFFFFFFF), // White
+          onSecondary: Color.fromARGB(255, 0, 0, 0), // Black
+          onBackground: Color(0xFFB0BEC5), // Light Gray
+          onSurface: Color(0xFFB0BEC5), // Light Gray
+        ),
+        textTheme: TextTheme(
+          displayLarge: TextStyle(color: Color(0xFF000000)), // Black
+          bodyLarge: TextStyle(color: Color(0xFF000000)), // Black
+        ),
         timePickerTheme: TimePickerThemeData(
           backgroundColor: Color(0xFFF8E1E9), // Light pastel pink
           hourMinuteTextColor: Colors.black,
@@ -48,17 +63,93 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: WakeUpTimeScreen(),
+      home: QuestionScreen(),
     );
   }
 }
 
-class WakeUpTimeScreen extends StatefulWidget {
+class QuestionScreen extends StatefulWidget {
   @override
-  _WakeUpTimeScreenState createState() => _WakeUpTimeScreenState();
+  _QuestionScreenState createState() => _QuestionScreenState();
 }
 
-class _WakeUpTimeScreenState extends State<WakeUpTimeScreen> {
+class _QuestionScreenState extends State<QuestionScreen> {
+  final PageController _pageController = PageController();
+  final List<Widget> _questions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _questions.addAll([
+      TimeQuestionScreen(
+        question: 'What time do you wake up in the morning?',
+        pageController: _pageController,
+        icon: Icons.wb_sunny, // Icon for waking up
+        isLast: false,
+      ),
+      TimeQuestionScreen(
+        question: 'At what time do you have breakfast?',
+        pageController: _pageController,
+        icon: Icons.free_breakfast, // Icon for breakfast
+        isLast: false,
+      ),
+      TimeQuestionScreen(
+        question: 'What time do you have lunch?',
+        pageController: _pageController,
+        icon: Icons.lunch_dining, // Icon for lunch
+        isLast: false,
+      ),
+      TimeQuestionScreen(
+        question: 'What time do you have dinner?',
+        pageController: _pageController,
+        icon: Icons.dinner_dining, // Icon for dinner
+        isLast: false,
+      ),
+      TimeQuestionScreen(
+        question: 'What time do you go to bed at night?',
+        pageController: _pageController,
+        icon: Icons.bedtime, // Icon for going to bed
+        isLast: true,
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 217, 242, 255),
+        title: Text('PillPoppin Questions'),
+      ),
+      backgroundColor: Color.fromARGB(255, 217, 242, 255),
+      body: PageView(
+        controller: _pageController,
+        children: _questions,
+        physics:
+            NeverScrollableScrollPhysics(), // Disable swipe to change pages
+      ),
+    );
+  }
+}
+
+class TimeQuestionScreen extends StatefulWidget {
+  final String question;
+  final PageController pageController;
+  final IconData icon;
+  final bool isLast;
+
+  TimeQuestionScreen({
+    required this.question,
+    required this.pageController,
+    required this.icon,
+    required this.isLast,
+  });
+
+  @override
+  _TimeQuestionScreenState createState() => _TimeQuestionScreenState();
+}
+
+class _TimeQuestionScreenState extends State<TimeQuestionScreen> {
   TimeOfDay _selectedTime = TimeOfDay(hour: 7, minute: 0);
 
   Future<void> _selectTime(BuildContext context) async {
@@ -76,6 +167,11 @@ class _WakeUpTimeScreenState extends State<WakeUpTimeScreen> {
             textButtonTheme: TextButtonThemeData(
               style: TextButton.styleFrom(
                 foregroundColor: Colors.black, // button text color
+                side:
+                    BorderSide(color: Color(0xFFF06292)), // button border color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
               ),
             ),
           ),
@@ -90,7 +186,6 @@ class _WakeUpTimeScreenState extends State<WakeUpTimeScreen> {
     }
   }
 
-  // Define a common text style
   TextStyle commonTextStyle = TextStyle(
     color: Colors.blueGrey[900],
     fontSize: 20,
@@ -100,153 +195,173 @@ class _WakeUpTimeScreenState extends State<WakeUpTimeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 217, 242, 255),
-      ),
-      backgroundColor: Color.fromARGB(255, 217, 242, 255),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'What time do you wake up in the morning?',
-                textAlign: TextAlign.center,
-                style: commonTextStyle,
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 40.0),
+            Container(
+              alignment: Alignment.topCenter,
+              child: Icon(
+                widget.icon,
+                size: 80.0, // Adjust the size of the icon
+                color: Colors.black, // Color of the icon
               ),
-              SizedBox(height: 20.0),
-              GestureDetector(
-                onTap: () => _selectTime(context),
-                child: Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Colors.white,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "${_selectedTime.hour.toString().padLeft(2, '0')}:${_selectedTime.minute.toString().padLeft(2, '0')}",
-                        style: TextStyle(
-                            fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 40.0),
+            Text(
+              widget.question,
+              textAlign: TextAlign.center,
+              style: commonTextStyle,
+            ),
+            SizedBox(height: 20.0),
+            GestureDetector(
+              onTap: () => _selectTime(context),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "${_selectedTime.hour.toString().padLeft(2, '0')}:${_selectedTime.minute.toString().padLeft(2, '0')}",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  if (widget.isLast) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  } else {
+                    widget.pageController.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
+                  }
+                },
+                borderRadius: BorderRadius.circular(30.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 244, 167, 193), // Pastel Pink
+                        Color(0xFFF06292), // Slightly darker Pastel Pink
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [0.1, 1.0],
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 4),
+                        blurRadius: 4.0,
+                        spreadRadius: 1.0,
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
                       Icon(
-                        Icons.arrow_drop_down,
+                        Icons.arrow_forward,
                         color: Colors.black,
+                      ),
+                      SizedBox(width: 8.0),
+                      Text(
+                        'Next',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
-              SizedBox(height: 30.0),
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () {
-                    // Handle next button press
-                  },
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromARGB(255, 244, 167, 193), // Pastel Pink
-                          Color(0xFFF06292), // Slightly darker Pastel Pink
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        stops: [0.1, 1.0],
+            ),
+            SizedBox(height: 20),
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  if (widget.pageController.page! > 0) {
+                    widget.pageController.previousPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
+                  }
+                },
+                borderRadius: BorderRadius.circular(30.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 244, 167, 193), // Pastel Pink
+                        Color(0xFFF06292), // Slightly darker Pastel Pink
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [0.1, 1.0],
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 4),
+                        blurRadius: 4.0,
+                        spreadRadius: 1.0,
                       ),
-                      borderRadius: BorderRadius.circular(30.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0, 4),
-                          blurRadius: 4.0,
-                          spreadRadius: 1.0,
-                        ),
-                      ],
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.arrow_forward,
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 8.0),
+                      Text(
+                        'Previous',
+                        style: TextStyle(
+                          fontSize: 18,
                           color: Colors.black,
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(width: 8.0),
-                        Text(
-                          'Next',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () {
-                    // Handle previous button press
-                  },
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromARGB(255, 244, 167, 193), // Pastel Pink
-                          Color(0xFFF06292), // Slightly darker Pastel Pink
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        stops: [0.1, 1.0],
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0, 4),
-                          blurRadius: 4.0,
-                          spreadRadius: 1.0,
-                        ),
-                      ],
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.arrow_back,
-                          color: Colors.black,
-                        ),
-                        SizedBox(width: 8.0),
-                        Text(
-                          'Previous',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
