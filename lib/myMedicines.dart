58e22acb33139d7d739201595c84cd8343fffae6
Ghaidaaazzam <gidaa.azzam@gmail.com>
2024-07-14 14:32:56 +0300

    update images in my medicines

--- lib/myMedicines.dart
+++ lib/myMedicines.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'UserProfile.dart'; // Import UserProfile
 import 'TakeORMiss.dart'; // Import MedicineIntakePage
 import 'HomePage.dart'; // Import HomePage
@@ -18,17 +20,40 @@ class MyMedicines extends StatefulWidget {
 class _MyMedicinesState extends State<MyMedicines> {
   int _selectedIndex = 2;
   late NotificationHelper notificationHelper;
+  Map<String, Map<String, String>> medicineDetails = {};
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     notificationHelper = NotificationHelper(context);
+    loadMedicineDetails();
   }
 
   @override
   void dispose() {
     notificationHelper.removeObserver();
     super.dispose();
+  }
+
+  Future<void> loadMedicineDetails() async {
+    final data = await rootBundle.loadString('Excels/Medicine.csv');
+    final lines = LineSplitter.split(data);
+    for (var line in lines.skip(1)) {
+      final values = line.split(',');
+      if (values.isNotEmpty && values.length > 7) {
+        medicineDetails[values[0]] = {
+          'form': values[1],
+          'unit': values[2],
+          'doseType': values[3],
+          'capacity': values[4],
+          'image': values[5],
+          'warning': values[6],
+          'sideEffect': values[7],
+        };
+      }
+    }
+    setState(() {});
+    print("Medicine details loaded: $medicineDetails"); // Debug print
   }
 
   void _onItemTapped(int index) {
@@ -130,6 +155,43 @@ class _MyMedicinesState extends State<MyMedicines> {
       context,
       MaterialPageRoute(
           builder: (context) => MedicineStock(userId: widget.userId)),
+    );
+  }
+
+  void _showEnlargedImage(String imagePath) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(imagePath, fit: BoxFit.contain),
+              SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Close',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black54,
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 
@@ -254,6 +316,27 @@ class _MyMedicinesState extends State<MyMedicines> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          if (medicineDetails
+                              .containsKey(prescription['medicineName']))
+                            GestureDetector(
+                              onTap: () {
+                                print(
+                                    "Image clicked: ${medicineDetails[prescription['medicineName']]!['image']!}");
+                                _showEnlargedImage(medicineDetails[
+                                    prescription['medicineName']]!['image']!);
+                              },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  medicineDetails[
+                                      prescription['medicineName']]!['image']!,
+                                  width: double.infinity,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          SizedBox(height: 8),
                           Text(
                             'Medicine: ${prescription['medicineName']}',
                             style: TextStyle(
